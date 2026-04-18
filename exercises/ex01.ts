0d7eb0e1eedd8690acd634bd288abda2aa3d260a
jamie <jamie927@gmail.com>
2026-04-17 18:53:47 -0700

    exercise 01

--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -9,8 +9,42 @@
 // Eg: printInfoOrCalculate("TS",5,"+") //Expected result: TS
 // Eg: printInfoOrCalculate("TS") //Expected result: TS
 
-const printInfoOrCalculate = () => {
-  // Code here
+enum Operator {
+  Add = "+",
+  Subtract = "-",
+  Multiply = "*",
+  Divide = "/"
+}
+
+const printInfoOrCalculate = (
+  p1: string | number,
+  p2?: number,
+  p3?: string
+): string | number => {
+  if (typeof p1 === "string") {
+    return p1;
+  }
+
+  if (typeof p1 === "number" && (p2 === undefined || p3 === undefined)) {
+    throw new Error("Invalid input");
+  }
+
+  if (typeof p1 === "number" && typeof p2 === "number" && p3) {
+    switch (p3) {
+      case Operator.Add:
+        return p1 + p2;
+      case Operator.Subtract:
+        return p1 - p2;
+      case Operator.Multiply:
+        return p1 * p2;
+      case Operator.Divide:
+        return p1 / p2;
+      default:
+        throw new Error("Invalid input");
+    }
+  }
+
+  return p1;
 };
 
 // Tests
